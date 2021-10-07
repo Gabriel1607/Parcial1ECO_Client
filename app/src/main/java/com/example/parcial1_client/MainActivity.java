1da8741fccaaf54f7b82fdba0d766673a413ec75
Gabriel <gabrielmora2001@hotmail.com>
2021-10-07 17:10:48 -0500

    Ya se manda la informacion, y no permite que mande eso vacio

--- app/src/main/java/com/example/parcial1_client/MainActivity.java
+++ app/src/main/java/com/example/parcial1_client/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.parcial1_client;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.InputFilter;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.parcial1_client.model.Particle;
 import com.google.gson.Gson;
@@ -19,6 +22,9 @@ private TextView titleGen,subTitle1,subTitle2,subTitle3,subTitle4;
 private EditText editName,editQuant,editX,editY;
 private Button redBtn,greenBtn,blueBtn,createBtn,deleteBtn;
 private TCPSingleton tcp;
+private float posX,posY;
+private int r,g,b,parNum;
+private String grupo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,25 +45,63 @@ private TCPSingleton tcp;
         deleteBtn = findViewById(R.id.deleteBtn);
         tcp = TCPSingleton.getInstance();
         createBtn.setOnClickListener(this);
-
+        redBtn.setOnClickListener(this);
+        greenBtn.setOnClickListener(this);
+        blueBtn.setOnClickListener(this);
+        editQuant.setInputType(2);
+        editQuant.setFilters(new InputFilter[] { new InputFilter.LengthFilter(2) });
+        editX.setInputType(2);
+        editX.setFilters(new InputFilter[] { new InputFilter.LengthFilter(3) });
+        editY.setInputType(2);
+        editY.setFilters(new InputFilter[] { new InputFilter.LengthFilter(3) });
 
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View view) {
         switch (view.getId()){
+            case R.id.redBtn:
+                redBtn.setText("✓");
+                greenBtn.setText("");
+                blueBtn.setText("");
+                 r = 193;
+                 g = 29;
+                 b = 29;
+                break;
+            case R.id.greenBtn:
+                redBtn.setText("");
+                greenBtn.setText("✓");
+                blueBtn.setText("");
+                r = 76;
+                g = 175;
+                b = 80;
+                break;
+            case R.id.blueBtn:
+
+                redBtn.setText("");
+                greenBtn.setText("");
+                blueBtn.setText("✓");
+                r = 63;
+                g = 81;
+                b = 181;
+                break;
             case R.id.createBtn:
-                Gson gson = new Gson();
-                float posX = 50.5F;
-                float posY = 12.5F;
-                int r= 255;
-                int g = 0;
-                int b = 0;
-                String grupo = "Grupo 1";
-                int parNum = 3;
-                Particle obj = new Particle(posX,posY,r,g,b,grupo,parNum);
-                String json = gson.toJson(obj);
-                tcp.sendMessage(json);
+                     if(!editName.getText().toString().replaceAll("\\s","").isEmpty()&&
+                        !editQuant.getText().toString().replaceAll("\\s","").isEmpty()&&
+                             !editX.getText().toString().replaceAll("\\s","").isEmpty()&&
+                             !editY.getText().toString().replaceAll("\\s","").isEmpty()) {
+                    Gson gson = new Gson();
+                    posX = Float.parseFloat(editX.getText().toString());
+                    posY = Float.parseFloat(editY.getText().toString());
+                    grupo = editName.getText().toString();
+                    parNum = Integer.parseInt(editQuant.getText().toString());
+                    Particle obj = new Particle(posX, posY, r, g, b, grupo, parNum);
+                    String json = gson.toJson(obj);
+                    tcp.sendMessage(json);
+                }else{
+                         Toast.makeText(this, "Por favor, llena todos los datos", Toast.LENGTH_SHORT).show();
+                     }
                 break;
         }
     }
